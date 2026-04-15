67a31047d41928ef58f1196215c1f86e033206f9
vaibhavchaudhary33 <141805893+vaibhavchaudhary33@users.noreply.github.com>
2026-04-15 16:02:40 +0000

    day 1 first code

---
+++ d1.cpp
@@ -0,0 +1,19 @@
+//day 1 of cpp 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+    for(j = 1; j <= 5; j = j + 1){
+    for(i = 1; i <= 5; i = i + 1)
+    {
+        cout << "* ";
+    }
+    
+    
+    cout << endl;
+    }
+    return 0;
+}
